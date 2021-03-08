9ff671d2af8aef9078035493e6ec4de6eee9caee
Toby Varland <toby.varland@varland.com>
2021-03-08 15:26:41 -0500

    Fix code error for Ruby 2.7.1

--- app/jobs/log_broadcast_job.rb
+++ app/jobs/log_broadcast_job.rb
@@ -3,7 +3,7 @@ class LogBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(log)
-    ActionCable.server.broadcast "live_channel", log: render_log(log), controller: log.controller_name
+    ActionCable.server.broadcast("live_channel", log: render_log(log), controller: log.controller_name)
   end
 
   private
