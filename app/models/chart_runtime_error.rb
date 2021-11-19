a60c1e9d735780907913820034ea97a758bfe237
Toby Varland <toby.varland@varland.com>
2021-11-19 17:19:47 -0500

    Set up chart runtime error

---
+++ app/models/chart_runtime_error.rb
@@ -0,0 +1,35 @@
+class ChartRuntimeError < Log
+
+  # Instance methods.
+
+  # Returns badge classes for log.
+  def badge_classes
+    return ["text-white", "bg-pink-500"]
+  end
+
+  # Returns notification settings for log.
+  def notification_settings
+    return {
+      enabled: true,
+      subject: "#{self.controller_name}: Chart Runtime Error",
+      recipients: [TOBY_VARLAND_EMAIL]
+    }
+  end
+
+  # Default details string.
+  def details
+    return "Chart running too long. Limit: <code>#{self.limit} seconds</code>. Runtime: <code>#{self.reading} seconds</code>."
+  end
+
+  # Returns headers for CSV file.
+  def csv_headers
+    return ["Date/Time", "Limit (sec)", "Runtime (sec)"]
+  end
+
+  # Returns data for CSV file.
+  def to_csv
+    attributes = %w{log_at limit reading}
+    return attributes.map {|attr| self.send(attr) }
+  end
+
+end
